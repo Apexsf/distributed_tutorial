a12f82c40eed0c3bcd71225008ac61d682dd1070
Apexsf <50563213+Apexsf@users.noreply.github.com>
2022-05-20 13:42:09 +0800

    add fixme for distributedsampler

--- src/mnist-distributed.py
+++ src/mnist-distributed.py
@@ -85,6 +85,7 @@ def train(gpu, args):
     start = datetime.now()
     total_step = len(train_loader)
     for epoch in range(args.epochs):
+        # FIXME: add 'trainsampler.set_epoch(epoch)'
         for i, (images, labels) in enumerate(train_loader):
             images = images.cuda(non_blocking=True)
             labels = labels.cuda(non_blocking=True)
